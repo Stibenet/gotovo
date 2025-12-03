4242eb609496135e7515b8bedaf73bb20bb8afca
Malkollm <fatkullinmars23@gmail.com>
2025-12-03 23:39:53 +0500

    Initial backend project

---
+++ gotovobackend/src/main/java/ru/gotovoweb/gotovobackend/GotovobackendApplication.java
@@ -0,0 +1,13 @@
+package ru.gotovoweb.gotovobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotovobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotovobackendApplication.class, args);
+	}
+
+}
---
+++ gotovobackend/src/test/java/ru/gotovoweb/gotovobackend/GotovobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gotovoweb.gotovobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotovobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
